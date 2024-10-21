83b9b5fe5c6aed32c0b528416b551aaf7b128762
LeeJaeHoon <dlwogns3413@naver.com>
2024-10-21 14:41:28 +0900

    refactor: sse 연결 hook 리팩터링

--- frontend/hooks/common/useConnectionSse.ts
+++ frontend/hooks/common/useConnectionSse.ts
@@ -2,6 +2,7 @@ import EventSource, { EventSourceListener } from 'react-native-sse';
 import { useEffect } from 'react';
 import 'react-native-url-polyfill/auto';
 import * as SecureStore from 'expo-secure-store';
+import useMe from '../queries/member/useMe';
 
 type CustomEvent =
   | 'CONNECT'
@@ -11,34 +12,46 @@ type CustomEvent =
   | 'schedule_completion';
 
 const useConnectionSse = () => {
-  useEffect(() => {
-    const url = new URL(`http://localhost:8080/sse/connect/house/${1}`);
-    let eventSource: EventSource<CustomEvent>;
-    SecureStore.getItemAsync('accessToken').then(token => {
-      eventSource = new EventSource<'CONNECT'>(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+  const { data: meData } = useMe();
 
-      const listener: EventSourceListener<CustomEvent> = event => {
-        if (event.type === 'open') {
-          console.log('Open SSE connection.');
-        } else if (event.type === 'error') {
-          console.error('Connection error:', event.message);
-        } else if (event.type === 'exception') {
-          console.error('Error:', event.message, event.error);
-        } else if (event.type === 'CONNECT') {
-          console.log('event ', event);
-        } else if (event.type === 'schedule_completion') {
-          console.log('event ', event);
-        }
-      };
-      eventSource.addEventListener('CONNECT', listener);
-      eventSource.addEventListener('open', listener);
-      eventSource.addEventListener('message', listener);
-      eventSource.addEventListener('error', listener);
-    });
+  useEffect(() => {
+    let eventSource: EventSource<CustomEvent> | null;
+
+    const initializeEventSource = async () => {
+      if (meData && meData.success) {
+        const { house } = meData.data;
+        const url = house
+          ? new URL(`http://localhost:8080/sse/connect/house/${house.houseId}`)
+          : new URL(`http://localhost:8080/sse/connect`);
+        const accessToken = await SecureStore.getItemAsync('accessToken');
+
+        eventSource = new EventSource<'CONNECT'>(url, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const listener: EventSourceListener<CustomEvent> = event => {
+          if (event.type === 'open') {
+            console.log('Open SSE connection.');
+          } else if (event.type === 'error') {
+            console.error('Connection error:', event.message);
+          } else if (event.type === 'exception') {
+            console.error('Error:', event.message, event.error);
+          } else if (event.type === 'CONNECT') {
+            console.log('event ', event);
+          } else if (event.type === 'schedule_completion') {
+            console.log('event ', event);
+          }
+        };
+        eventSource.addEventListener('CONNECT', listener);
+        eventSource.addEventListener('open', listener);
+        eventSource.addEventListener('message', listener);
+        eventSource.addEventListener('error', listener);
+      }
+    };
+
+    initializeEventSource();
 
     return () => {
       if (eventSource != null) {
@@ -46,7 +59,7 @@ const useConnectionSse = () => {
         eventSource.close();
       }
     };
-  }, []);
+  }, [meData]);
 };
 
 export default useConnectionSse;
